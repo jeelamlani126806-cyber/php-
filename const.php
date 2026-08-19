dd3aa2519d93cd2473d0923df2f1728cb3488078
jeelamlani126806-cyber <jeel.amlani126806@marwadiuniversity.ac.in>
2026-08-19 23:23:59 +0530

    Add files via upload

---
+++ const.php
@@ -0,0 +1,14 @@
+<?php
+
+    define("COLLEGE", " College"); 
+    
+    $name = "jeel";      
+    $marks = 450;
+    $result ="pass";
+    
+    echo "<h3>Previous Semester Result</h3>";
+    echo "College: " . COLLEGE . "<br>";
+    echo "Name: " . $name . "<br>";
+    echo "Marks: " . $marks;
+    echo "result" .$result;
+    ?>
