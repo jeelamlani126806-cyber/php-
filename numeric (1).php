c1b8141fe12b87294d16890bf86fc389f2d975a6
jeelamlani126806-cyber <jeel.amlani126806@marwadiuniversity.ac.in>
2026-08-19 23:46:17 +0530

    Add files via upload

---
+++ numeric (1).php
@@ -0,0 +1,41 @@
+<?php
+$days = array("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+echo "<h3>Numeric Array:</h3>";
+foreach ($days as $day) {
+    echo $day . "<br>";
+}
+$months = array(
+    "January" => 31,
+    "February" => 28,
+    "March" => 31,
+    "April" => 30,
+    "May" => 31,
+    "June" => 30,
+    "July" => 31,
+    "August" => 31,
+    "September" => 30,
+    "October" => 31,
+    "November" => 30,
+    "December" => 31
+);
+echo "<h3>Associative Array:</h3>";
+foreach ($months as $month => $days) {
+    echo $month . " => " . $days . "<br>";
+}
+
+$laptops = array(
+    "Dell" => array(
+        "Model" => "Inspiron 15",
+        "Price" => 55000
+    ),
+    "HP" => array(
+        "Model" => "Pavilion 14",
+        "Price" => 60000
+    )
+);
+echo "<h3>Multidimensional Array:</h3>";
+foreach ($laptops as $company => $details) {
+    echo "Company: " . $company . "<br>";
+    echo "Model: " . $details["Model"] . "<br>";
+    echo "Price: ₹" . $details["Price"] . "<br><br>";}
+?>
